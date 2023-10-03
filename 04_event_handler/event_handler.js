9914738ac6fef82b805f9e760c95c2183edf6ee3
rosamazela <rosamazela6@gmail.com>
2023-10-03 09:42:36 +0700

    finalisasi tugas 1 praktikum PAB

---
+++ 04_event_handler/event_handler.js
@@ -0,0 +1,33 @@
+// Button Component
+class Tombol extends React.Component {
+    handleButtonClick = () => {
+        alert(this.props.message);
+    }
+
+    render() {
+    return (
+        <button onClick={handleButtonClick} style={{ margin: "10px" }}>
+            {this.props.children}
+        </button>
+    )
+    }
+}
+
+// App Component
+class MyApp extends React.Component {
+    render() {
+    return (
+        <div>
+            <h1>Learn React</h1>
+            <MyButton message="Learn React">React</MyButton>
+            <MyButton message="Learn JavaScript">JavaScript</MyButton>
+        </div>
+    )
+    }
+}
+
+const myElement = <MyApp />;
+const myApp = myElement;
+
+ReactDOM.createRoot(document.getElementById('root')).render(myApp);
+
